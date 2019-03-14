57faaa55733ec327bca5efca8b594ced4aac4935
Glimpson <glimpseofmetal@gmail.com>
2019-03-15 01:24:42 +0200

    Home page updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  // { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './remove-transfer/remove-transfer.module#RemoveTransferPageModule' },
   { path: 'remove-transfer', loadChildren: './remove-transfer/remove-transfer.module#RemoveTransferPageModule' },
   { path: 'driver-list', loadChildren: './driver-list/driver-list.module#DriverListPageModule' },
   { path: 'results', loadChildren: './results/results.module#ResultsPageModule' }
